d015480f81e0be8b4beced3259a9509eb8c0be7b
vsoch <vsoch@users.noreply.github.com>
2025-09-25 10:24:44 -0700

    wip: scaling agent
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- fractale/agent/scaling/agent.py
+++ fractale/agent/scaling/agent.py
@@ -20,22 +20,16 @@ from fractale.agent.decorators import timed
 from fractale.agent.results import ResultParser
 
 
-class OptimizationAgent(GeminiAgent):
+class ScalingAgent(GeminiAgent):
     """
-    Optimization Agent
-
-    The optimization agent receives a figure of merit request
-    and a need to minimize or maximize. It will return to a job
-    running agent (akin to Kubernetes Job) that has explicit
-    instructions to only change a subset of the execution arguments.
-    This means that the optimization agent can be general to take
-    some application and parameters attempted before. The optimization
-    agent should return a new parameter set to the build agent.
+    Scaling Agent
+    
+    The scaling agent is responsible for orchestrating a scaling study.
     """
 
-    name = "optimization"
-    description = "optimization agent"
-    state_variables = ["optimize"]
+    name = "scaling agent"
+    description = "scaling study agent"
+    state_variables = ["scale", "sizes"]
 
     def init(self):
         self.model = genai.GenerativeModel(defaults.gemini_model)
@@ -46,15 +40,10 @@ class OptimizationAgent(GeminiAgent):
             sys.exit("ERROR: GEMINI_API_KEY environment variable not set.")
 
         # We will save figures of merit, and attempt crds
-        self.metadata["assets"]["updates"] = []
-        self.metadata["assets"]["regex"] = []
-        self.metadata["assets"]["regex-attempts"] = []
-        self.foms = []
+        self.metadata["assets"]["results"] = {}
 
-        # We could just take the listing of FOMs, but I am not sure
-        # if there are cases where that might not reflect attempts.
-        self.metadata["optimize_attempts"] = 0
-        self.parser = ResultParser()
+        # Keep a lookup of scale attempts by size
+        self.metadata["scale_attempts"] = {}
 
     def _add_arguments(self, subparser):
         """
@@ -66,49 +55,33 @@ class OptimizationAgent(GeminiAgent):
             description=self.description,
         )
         agent.add_argument(
-            "optimize",
-            help="Optimization instruction (include application, environment, algorithm, etc.).",
-        )
-        agent.add_argument(
-            "function",
-            help="Request an optimization function instead",
-            action="store_true",
-            default=False,
+            "instruction",
+            help="Scaling instruction (include application, environment, algorithm, etc.).",
         )
         return agent
 
     @timed
-    def run(self, context, log, prompt=None, additional=""):
+    def run(self, context):
         """
-        Run the optimization agent.
+        Run the scaling agent.
 
         Additional text can reset each time.
         """
         # We don't do attempts because we have no condition for success.
         context = get_context(context)
 
-        prompt = prompt or context.get("requires")
-
         # If requirements not specified, we require the "optimize" context
-        if context.get("function") and self.metadata["optimize_attempts"] == 0:
-            prompt = prompts.get_initial_function_optimize_prompt(context)
-        elif context.get("function"):
-            prompt = prompts.get_function_optimize_prompt(context)
-        elif not prompt:
-            prompt = prompts.get_optimize_prompt(context)
+        prompt = prompts.get_scaling_prompt(context)
 
         # Parser requires is the FOM and optimize directive.
         # This returns a list of foms.
         foms = self.parser.parse(context.optimize, log, context.get("optimize.regex"))
         self.foms += foms
-        self.metadata["optimize_attempts"] += 1
 
-        # This adds supplementary detail about how to optimize - "keep going until it's good":_
-        additional = additional + "\n" if additional else ""
-        prompt += additional + prompts.supplement_optimize_prompt % (
-            self.metadata["optimize_attempts"],
-            json.dumps(self.foms),
-        )
+        # Keep track of how many tries we do
+        if context.size not in self.metadata['scaling_attempts']:
+            self.metadata['scaling_attempts'][context.size] = 0
+        self.metadata["scaling_attempts"][context.size] += 1
 
         # TODO: if this agent stores memory we don't need to include dockerfile after the first...
         print("Sending optimization prompt to Gemini...")
@@ -130,11 +103,6 @@ class OptimizationAgent(GeminiAgent):
         # This is an invalid result.
         if "decision" not in result or "reason" not in result:
             additional = "The JSON MUST have the fields 'decision', 'reason' at the top level with the manifest."
-            return self.run(context, log, prompt, additional)
-
-        # This makes it easier for other agents to identify
-        if result['decision'] == "STOP" and "final" not in result:
-            additional = "When you STOP you MUST include the 'final' result that is optimized."
             return self.run(context, log, prompt, additional)
 
         # We can't be sure of the format or how to update, so return to job agent

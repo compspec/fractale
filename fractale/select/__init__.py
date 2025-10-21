c5c3916f0aedd2be903f27fb9fb3f233094acf4f
vsoch <vsoch@users.noreply.github.com>
2025-10-21 14:09:20 -0700

    bug: genai import from cost selector
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- fractale/select/__init__.py
+++ fractale/select/__init__.py
@@ -1,7 +1,16 @@
-from .cost import CostSelection
 from .random import RandomSelection
 
-algorithms = {"random": RandomSelection, "agent-cost": CostSelection}
+algorithms = {"random": RandomSelection}
+
+# These require genai
+try:
+    import google.generativeai as genai
+
+    from .cost import CostSelection
+
+    algorithms["agent-cost"] = CostSelection
+except ImportError:
+    pass
 
 
 def get_selector(name):
